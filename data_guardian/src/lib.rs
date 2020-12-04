66638763aa676fcd38d824d0b47cddae2a3439a2
Damian J Pound <damian@thebestisp.com>
2020-12-03 20:17:38 -0600

    Remove unnecessary wasm-bindgen

--- data_guardian/src/lib.rs
+++ data_guardian/src/lib.rs
@@ -1,18 +1,17 @@
 use std::collections::HashMap;
 use std::ptr::NonNull;
-use wasm_bindgen::prelude::*;
 
 pub enum Guardian {
     Data(u32),
     Map(HashMap<u32, Guardian>),
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_new() -> *mut Guardian {
     Box::into_raw(Box::new(Guardian::Map(HashMap::new())))
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_free(guardian: *mut Guardian) -> bool {
     if !guardian.is_null() {
         unsafe { Box::from_raw(guardian) };
@@ -22,7 +21,7 @@ pub fn guardian_free(guardian: *mut Guardian) -> bool {
     }
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_which(guardian: *mut Guardian) -> u8 {
     if let Some(mut x) = NonNull::new(guardian) {
         match unsafe { x.as_mut() } {
@@ -34,7 +33,7 @@ pub fn guardian_which(guardian: *mut Guardian) -> u8 {
     }
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_get_data(guardian: *mut Guardian) -> u32 {
     (|| {
         if let Guardian::Data(map) = unsafe { NonNull::new(guardian)?.as_ref() } {
@@ -46,7 +45,7 @@ pub fn guardian_get_data(guardian: *mut Guardian) -> u32 {
     .unwrap_or(0)
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_set_data(guardian: *mut Guardian, data: u32) -> bool {
     (|| {
         *unsafe { NonNull::new(guardian)?.as_mut() } = Guardian::Data(data);
@@ -55,7 +54,7 @@ pub fn guardian_set_data(guardian: *mut Guardian, data: u32) -> bool {
     .unwrap_or_default()
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_get_map_elem(guardian: *mut Guardian, key: u32) -> *mut Guardian {
     (|| {
         if let Guardian::Map(map) = unsafe { NonNull::new(guardian)?.as_mut() } {
@@ -67,7 +66,7 @@ pub fn guardian_get_map_elem(guardian: *mut Guardian, key: u32) -> *mut Guardian
     .unwrap_or(std::ptr::null_mut())
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_detach_map_elem(guardian: *mut Guardian, key: u32) -> *mut Guardian {
     (|| {
         if let Guardian::Map(map) = unsafe { NonNull::new(guardian)?.as_mut() } {
@@ -79,7 +78,7 @@ pub fn guardian_detach_map_elem(guardian: *mut Guardian, key: u32) -> *mut Guard
     .unwrap_or(std::ptr::null_mut())
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_delete_map_elem(guardian: *mut Guardian, key: u32) -> bool {
     (|| {
         if let Guardian::Map(map) = unsafe { NonNull::new(guardian)?.as_mut() } {
@@ -91,7 +90,7 @@ pub fn guardian_delete_map_elem(guardian: *mut Guardian, key: u32) -> bool {
     .unwrap_or_default()
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn guardian_replace(guardian: *mut Guardian, map: *mut Guardian) -> bool {
     if map.is_null() {
         return false;
